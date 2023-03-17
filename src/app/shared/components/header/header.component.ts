257a22a6f73d2353dc67d2a95b331e826f60bc96
patskal <patskalWork@gmail.com>
2023-03-17 19:08:53 +0200

    Added material lib and implemented header
    * created new folder "icons" in assets
    * added new icons for header and title-icon
    * installed material library
    * implemented header component
    * added style files for store variables
    * updated reset file

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  public isMainPage: boolean;
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.isMainPage = this.router.url === '/';
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, MatIconModule],
+  exports: [HeaderComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
