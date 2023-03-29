20a86cbc0520ac21b2073a3eb3a45e466ca24931
patskal <patskalWork@gmail.com>
2023-03-29 19:21:52 +0300

    Adapted New-Products component
    * Adapted New-Products component
    * Updated button styles

--- src/app/features/landing/components/new-products/new-products.component.ts
+++ src/app/features/landing/components/new-products/new-products.component.ts
@@ -13,10 +13,28 @@ export class NewProductsComponent {
   public config: SwiperOptions = {
     freeMode: true,
     mousewheel: true,
-    slidesPerView: 2.5,
-    spaceBetween: 50,
+    slidesPerView: 1,
+    spaceBetween: 25,
     scrollbar: {
       hide: true
+    },
+    breakpoints: {
+      380: {
+        slidesPerView: 1.2
+      },
+      500: {
+        slidesPerView: 1.5
+      },
+      800: {
+        slidesPerView: 1.8
+      },
+      1050: {
+        slidesPerView: 2.2
+      },
+      1300: {
+        slidesPerView: 2.5,
+        spaceBetween: 50
+      }
     }
   };
 }
