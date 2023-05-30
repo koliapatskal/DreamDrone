07cf3c11b00596f71065269b521d45c95b4c9685
patskal <patskalWork@gmail.com>
2023-05-30 10:02:41 +0300

    Removed home components

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -3,24 +3,10 @@ import { SwiperModule } from 'swiper/angular';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { GetNowComponent } from './components/get-now/get-now.component';
-import { NewProductsComponent } from './components/new-products/new-products.component';
-import {
-  PhotoDescriptionComponent
-} from './components/photo-description/photo-description.component';
-import { ReviewsComponent } from './components/reviews/reviews.component';
-import { WhyThisDroneComponent } from './components/why-this-drone/why-this-drone.component';
 import { LandingComponent } from './landing.component';
 
 @NgModule({
-  declarations: [
-    LandingComponent,
-    GetNowComponent,
-    PhotoDescriptionComponent,
-    WhyThisDroneComponent,
-    NewProductsComponent,
-    ReviewsComponent
-  ],
-  imports: [CommonModule, SwiperModule]
+  declarations: [LandingComponent],
+  imports: [CommonModule, SwiperModule],
 })
 export class LandingModule {}
