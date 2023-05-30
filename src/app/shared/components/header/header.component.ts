6c52deb830ba9e8a34d967c55ea021446b8d7219
patskal <patskalWork@gmail.com>
2023-05-30 16:04:55 +0300

    Reworked the header for the new design

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { SidenavService } from '../../services/sidenav/sidenav.service';
@@ -10,7 +10,20 @@ import { SidenavService } from '../../services/sidenav/sidenav.service';
 })
 export class HeaderComponent implements OnInit {
   public isMainPage: boolean;
+
+  @HostBinding('class.scrolled') isScrolled: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onscroll(): void {
+    if (window.scrollY > 10) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
+
   constructor(private router: Router, private sidenavService: SidenavService) {}
+
   public ngOnInit(): void {
     this.isMainPage = this.router.url === '/';
   }
