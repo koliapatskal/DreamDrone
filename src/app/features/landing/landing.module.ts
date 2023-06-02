e27f898cdfe60edf73e2e227e98e86408f61daba
patskal <patskalWork@gmail.com>
2023-06-02 13:19:52 +0300

    Implemented category list component
    * Added html structure
    * Added styles
    * Added photos for categories
    * Updated image paths in the json file

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -1,14 +1,16 @@
 import { SwiperModule } from 'swiper/angular';
 
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
+import { CategoryListComponent } from './components/category-list/category-list.component';
 import { HeaderVideoComponent } from './components/header-video/header-video.component';
 import { LandingComponent } from './landing.component';
 
 @NgModule({
-  declarations: [LandingComponent, HeaderVideoComponent],
-  imports: [CommonModule, SwiperModule, MatIconModule]
+  declarations: [LandingComponent, HeaderVideoComponent, CategoryListComponent],
+  imports: [CommonModule, SwiperModule, MatIconModule, HttpClientModule]
 })
 export class LandingModule {}
